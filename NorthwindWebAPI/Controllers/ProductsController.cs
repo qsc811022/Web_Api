8d3636230db2d6002dd7c9c63f5cecade4c346a5
劉敦桐 <zse811022@hotmail.com>
2023-12-15 21:02:42 +0800

    Delete have problom

--- NorthwindWebAPI/Controllers/ProductsController.cs
+++ NorthwindWebAPI/Controllers/ProductsController.cs
@@ -38,5 +38,65 @@ namespace NorthwindWebAPI.Controllers
 			return product;
 		}
 
+		// POST: api/products
+		[HttpPost]
+		public async Task<ActionResult<Product>> PostProduct(Product product)
+		{
+			_context.Products.Add(product);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
+		}
+
+		// PUT: api/products/5
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutProduct(int id, Product product)
+		{
+			if (id != product.ProductId)
+			{
+				return BadRequest();
+			}
+
+			_context.Entry(product).State = EntityState.Modified;
+
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!ProductExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return NoContent();
+		}
+		// DELETE: api/products/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteProduct(int id)
+		{
+			var product = await _context.Products.FindAsync(id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			_context.Products.Remove(product);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+		private bool ProductExists(int id)
+		{
+			return _context.Products.Any(e => e.ProductId == id);
+		}
+
+
 	}
 }
