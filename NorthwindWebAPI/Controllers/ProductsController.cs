73f9068dd902ca14f4af1cd447c0d55185865358
劉敦桐 <zse811022@hotmail.com>
2023-12-15 19:57:10 +0800

    list

---
+++ NorthwindWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using NorthwindWebAPI.Models;
+
+namespace NorthwindWebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly NorthwindContext _context;
+
+		public ProductsController(NorthwindContext context)
+		{
+			_context = context;
+		}
+
+		// GET: api/Products
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+		{
+			return await _context.Products.ToListAsync();
+		}
+
+	}
+}
