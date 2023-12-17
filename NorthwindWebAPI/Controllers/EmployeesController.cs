a3cc0000f4b6c1c66e251f8ca4390e6666a55b6c
劉敦桐 <zse811022@hotmail.com>
2023-12-17 15:00:02 +0800

    Add Employees Regions Controller

---
+++ NorthwindWebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using NorthwindWebAPI.Models;
+
+namespace NorthwindWebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EmployeesController : ControllerBase
+	{
+		private readonly NorthwindContext _context;
+
+		public EmployeesController(NorthwindContext context)
+		{
+			_context = context;
+		}
+
+		// GET: api/employees
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+		{
+			return await _context.Employees.ToListAsync();
+		}
+
+		// GET: api/employees/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Employee>> GetEmployee(int id)
+		{
+			var employee = await _context.Employees.FindAsync(id);
+
+			if (employee == null)
+			{
+				return NotFound();
+			}
+
+			return employee;
+		}
+
+		// POST: api/employees
+		[HttpPost]
+		public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+		{
+			_context.Employees.Add(employee);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction(nameof(GetEmployee), new { id = employee.EmployeeId }, employee);
+		}
+
+		// PUT: api/employees/5
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutEmployee(int id, Employee employee)
+		{
+			if (id != employee.EmployeeId)
+			{
+				return BadRequest();
+			}
+
+			_context.Entry(employee).State = EntityState.Modified;
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+
+		// DELETE: api/employees/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteEmployee(int id)
+		{
+			var employee = await _context.Employees.FindAsync(id);
+			if (employee == null)
+			{
+				return NotFound();
+			}
+
+			_context.Employees.Remove(employee);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+	}
+}
---
+++ NorthwindWebAPI/Controllers/RegionController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using NorthwindWebAPI.Models;
+
+namespace NorthwindWebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RegionsController : ControllerBase
+	{
+		private readonly NorthwindContext _context;
+
+		public RegionsController(NorthwindContext context)
+		{
+			_context = context;
+		}
+
+		// GET: api/regions
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Region>>> GetRegions()
+		{
+			return await _context.Regions.ToListAsync();
+		}
+
+		// GET: api/regions/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Region>> GetRegion(int id)
+		{
+			var region = await _context.Regions.FindAsync(id);
+
+			if (region == null)
+			{
+				return NotFound();
+			}
+
+			return region;
+		}
+
+		// POST: api/regions
+		[HttpPost]
+		public async Task<ActionResult<Region>> PostRegion(Region region)
+		{
+			_context.Regions.Add(region);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction(nameof(GetRegion), new { id = region.RegionId }, region);
+		}
+
+		// PUT: api/regions/5
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutRegion(int id, Region region)
+		{
+			if (id != region.RegionId)
+			{
+				return BadRequest();
+			}
+
+			_context.Entry(region).State = EntityState.Modified;
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+
+		// DELETE: api/regions/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteRegion(int id)
+		{
+			var region = await _context.Regions.FindAsync(id);
+			if (region == null)
+			{
+				return NotFound();
+			}
+
+			_context.Regions.Remove(region);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+	}
+}
